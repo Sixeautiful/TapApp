bf945f0a71c1b638488fd1d4c42cef3c38af4706
Sixeautiful <vladerastatkevich2015@gmail.com>
2022-03-01 15:25:12 +0100

    Initial Commit

---
+++ TapApp/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  TapApp
+//
+//  Created by Kirill Willer on 1.03.22.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ TapApp/TapAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  TapAppApp.swift
+//  TapApp
+//
+//  Created by Kirill Willer on 1.03.22.
+//
+
+import SwiftUI
+
+@main
+struct TapAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
